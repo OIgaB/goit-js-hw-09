6396cba3d2c12f81909a32bd4623a22c07a4187d
OIgaB <linkbolya@gmail.com>
2023-03-21 03:43:13 +0200

    complete the 1st task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+"use strict";
 
+const refs = {
+    startBtn: document.querySelector('button[data-start]'),
+    stopBtn: document.querySelector('button[data-stop]'),
+    body: document.querySelector('body'),
+}
+
+refs.startBtn.addEventListener('click', handleStartBtnClick);
+refs.stopBtn.addEventListener('click', handleStopBtnClick);
+
+let color = undefined;
+let colorSwitcher = undefined;
+
+function handleStartBtnClick(evt) {
+    if (evt.target.nodeName !== 'BUTTON') {
+        return;
+    }
+    colorSwitcher = setInterval(() => {
+        color = getRandomHexColor();
+        refs.body.style.backgroundColor = color;
+    }, 1000);
+
+    evt.target.disabled = true;
+}
+
+function handleStopBtnClick() {
+    clearInterval(colorSwitcher);
+    refs.startBtn.disabled = false;
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
